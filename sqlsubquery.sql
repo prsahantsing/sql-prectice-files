fd9131a2f731d50e0c78d509fa1fd7b3330d62aa
prashant <prashantsing798@gmail.com>
2025-06-22 12:54:03 +0530

    a new file has been added in the repository

---
+++ sqlsubquery.sql
@@ -0,0 +1,94 @@
+-- Departments
+CREATE TABLE Departmentss (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100)
+);
+
+-- Employees
+CREATE TABLE Employeess (
+    EmployeeID INT PRIMARY KEY,
+    FullName VARCHAR(100),
+    DepartmentID INT,
+    Salary DECIMAL(10,2),
+    PerformanceScore INT,
+    ManagerID INT NULL,
+    FOREIGN KEY (DepartmentID) REFERENCES Departmentss(DepartmentID),
+    FOREIGN KEY (ManagerID) REFERENCES Employeess(EmployeeID)
+);
+
+-- Sample data
+INSERT INTO Departmentss VALUES 
+(1, 'Sales'),
+(2, 'Engineering'),
+(3, 'HR'),
+(4, 'Finance');
+
+INSERT INTO Employeess VALUES 
+(1, 'Alice Johnson', 1, 75000, 90, NULL),  -- Sales Manager
+(2, 'Bob Smith', 1, 50000, 88, 1),
+(3, 'Carol Lee', 1, 52000, 82, 1),
+(4, 'David Kim', 2, 95000, 91, NULL),      -- Eng Manager
+(5, 'Eva Brown', 2, 70000, 94, 4),
+(6, 'Frank Hall', 2, 72000, 96, 4),
+(7, 'Grace Wu', 3, 60000, 75, NULL),       -- HR Manager
+(8, 'Hank Green', 3, 45000, 73, 7),
+(9, 'Ivy Chen', 4, 80000, 87, NULL);       -- Finance Manager
+
+
+select * from employeess
+select * from departmentss
+
+--? 1. CTE – Find Top Performer in Each Department
+
+
+
+with cte_performancerating as 
+(
+select 
+        employeeid , 
+        fullname , departmentid , 
+        salary , performancescore ,
+        rank() over (partition by departmentid order by performancescore desc ) performancerank 
+        from employeess
+  ) 
+        select employeeid , fullname , departmentid , salary , performancescore 
+    , performancerank 
+    from cte_performancerating
+    where performancerank = 1;
+
+    --? 2. Correlated Subquery – Employees Earning More Than Dept Avg
+
+with cte_avgdepartmentsalary as 
+(
+    select departmentid , avg(salary) as avgsalaryperdept
+    from employeess
+    group by departmentid 
+)
+        select e.employeeid , e.salary ,e.departmentid
+        from employeess as e
+        join cte_avgdepartmentsalary as ads
+        on ads.departmentid = e.departmentid 
+        where salary > avgsalaryperdept
+
+
+select
+    e1.employeeid , e1.salary , e1.DepartmentID 
+    from employeess as e1
+    where e1.salary > 
+            (
+                select avg(e2.salary) 
+                from employeess as e2
+                where e2.DepartmentID = e1.DepartmentID )
+
+
+--? 3. Correlated Subquery – Managers Earning Less Than Their Subordinates
+
+select *  from employeess 
+select * from departmentss
+
+select e1.managerid , e1.salary 
+from employeess as e1 
+where 1.salary < 
+        ( select min(e2.salary ) 
+        from employeess as e2 
+        where e2.employeeid = e1.employeeid )
